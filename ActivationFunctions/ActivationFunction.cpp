ed01ccc5cb2abd134fa25fe6f40f286e11729ad1
chimort <chimort@mail.ru>
2024-12-29 22:39:33 +0300

    made interaction between cmakelists

--- ActivationFunctions/ActivationFunction.cpp
+++ ActivationFunctions/ActivationFunction.cpp
@@ -5,7 +5,7 @@ namespace neural_network
 double ReLU::evaluate(const double& x) const { return x > 0 ? x : 0; }
 double ReLU::derEvaluate(const double& x) const { return x > 0 ? 1 : 0; }
 
-Matrix ReLU::evaluate(const Matrix& x) const { x.cwiseMax(0); }
-Matrix ReLU::derEvaluate(const Matrix& x) const { x.cwiseGreater(0).cast<double>(); }
+Matrix ReLU::evaluate(const Matrix& x) const { return x.cwiseMax(0); }
+Matrix ReLU::derEvaluate(const Matrix& x) const { return x.cwiseGreater(0).cast<double>(); }
 
 }  // namespace neural_network
