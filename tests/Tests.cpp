488ed5892d7eed5baef6e30c90f0311215921d50
chimort <chimort@mail.ru>
2025-03-06 14:29:54 +0300

    tetst

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -233,10 +233,193 @@ void test_mnist_classification()
     std::cout << "\nТочность на тестовом наборе: " << test_accuracy << "\n";
 }
 
+void test_mnist_classification_momentum()
+{
+    std::cout << "----- Тест: MNIST (классификация цифр 0-9) с оптимизатором momentum -----\n";
+
+    constexpr Index input_size = 28 * 28;
+    constexpr Index hidden_size = 128;
+    constexpr Index output_size = 10;
+    int num_train, train_rows, train_cols;
+
+    Matrix X_train = loadMNISTImages("mnist_dataset/train-images.idx3-ubyte", num_train, train_rows,
+                                     train_cols);
+    Matrix Y_train = loadMNISTLabels("mnist_dataset/train-labels.idx1-ubyte", num_train);
+
+    std::cout << "Тренировочный набор: Загружено " << num_train << " изображений размером "
+              << train_rows << "x" << train_cols << "\n";
+
+    ActivationFunction hiddenActivation = ActivationFunction::ReLU();
+    ActivationFunction outputActivation = ActivationFunction::Identity();
+
+    Optimizer momentumOpt = Optimizer::Momentum(0.99, 0.9);
+
+    Net net;
+    net.addLayer(AnyLayer::createDenseLayer(In{input_size}, Out{hidden_size}, hiddenActivation,
+                                            momentumOpt));
+    net.addLayer(AnyLayer::createDenseLayer(In{hidden_size}, Out{output_size}, outputActivation,
+                                            momentumOpt));
+
+    std::cout << "\n=== До обучения (тренировочный набор, первые 5 примеров) ===\n";
+    Matrix pred_train_before = net.predict(X_train.leftCols(5));
+    std::cout << "Предсказания (вероятностное распределение):\n"
+              << pred_train_before.transpose() << "\n";
+
+    std::cout << "Реальные метки: ";
+    for (int i = 0; i < 5; ++i) {
+        int real_label = oneHotToLabel(Y_train.col(i));
+        std::cout << real_label << " ";
+    }
+    std::cout << "\nПредсказанные метки: ";
+    for (int i = 0; i < 5; ++i) {
+        int predicted_label = oneHotToLabel(pred_train_before.col(i));
+        std::cout << predicted_label << " ";
+    }
+    std::cout << "\n";
+
+    constexpr int epochs = 10;
+    net.fit(X_train, Y_train, epochs, 32, momentumOpt, LossFunction::Euclid(),
+            Net::NormStatus::Active);
+
+    std::cout << "\n=== После обучения (тренировочный набор, первые 5 примеров) ===\n";
+    Matrix pred_train_after = net.predict(X_train.leftCols(5));
+    std::cout << "Предсказания (вероятностное распределение):\n"
+              << pred_train_after.transpose() << "\n";
+
+    std::cout << "Реальные метки: ";
+    for (int i = 0; i < 5; ++i) {
+        int real_label = oneHotToLabel(Y_train.col(i));
+        std::cout << real_label << " ";
+    }
+    std::cout << "\nПредсказанные метки: ";
+    for (int i = 0; i < 5; ++i) {
+        int predicted_label = oneHotToLabel(pred_train_after.col(i));
+        std::cout << predicted_label << " ";
+    }
+    std::cout << "\n";
+
+    double train_accuracy = net.accuracy(X_train, Y_train);
+    std::cout << "\nТочность на тренировочном наборе: " << train_accuracy << "\n";
+
+    int num_test, test_rows, test_cols;
+    Matrix X_test = loadMNISTImages("mnist_dataset/t10k-images.idx3-ubyte", num_test, test_rows,
+                                    test_cols);
+    Matrix Y_test = loadMNISTLabels("mnist_dataset/t10k-labels.idx1-ubyte", num_test);
+
+    std::cout << "\nТестовый набор: Загружено " << num_test << " изображений размером " << test_rows
+              << "x" << test_cols << "\n";
+
+    std::cout << "\n=== Итоговые предсказания (тестовый набор, первые 5 примеров) ===\n";
+    Matrix pred_test = net.predict(X_test.leftCols(5));
+    std::cout << "Предсказания (вероятностное распределение):\n" << pred_test.transpose() << "\n";
+
+    std::cout << "Реальные метки: ";
+    for (int i = 0; i < 5; ++i) {
+        int real_label = oneHotToLabel(Y_test.col(i));
+        std::cout << real_label << " ";
+    }
+    std::cout << "\nПредсказанные метки: ";
+    for (int i = 0; i < 5; ++i) {
+        int predicted_label = oneHotToLabel(pred_test.col(i));
+        std::cout << predicted_label << " ";
+    }
+    std::cout << "\n";
+
+    double test_accuracy = net.accuracy(X_test, Y_test);
+    std::cout << "\nТочность на тестовом наборе: " << test_accuracy << "\n";
+}
+
+void test_linear_regression_with_momentum()
+{
+    std::cout << "----- Тест: Линейная регрессия (y = 3x + 5) с оптимизатором Momentum -----\n";
+
+    constexpr Index input_size = 1;
+    constexpr Index hidden_size = 3;
+    constexpr Index output_size = 1;
+    constexpr Index num_samples = 5;
+
+    Eigen::Matrix<double, input_size, num_samples> X;
+    Eigen::Matrix<double, output_size, num_samples> Y;
+
+    X << 0.0, 1.0, 2.0, 3.0, 4.0;
+    Y << 5.0, 8.0, 11.0, 14.0, 17.0;
+
+    std::cout << "Входные данные X:\n" << X << "\n\n";
+    std::cout << "Целевые данные Y:\n" << Y << "\n\n";
+
+    ActivationFunction activation = ActivationFunction::Identity();
+
+    Optimizer momentumOpt = Optimizer::Momentum(0.5, 0.9);
+    Net netMomentum;
+    netMomentum.addLayer(
+        AnyLayer::createDenseLayer(In{input_size}, Out{hidden_size}, activation, momentumOpt));
+    netMomentum.addLayer(
+        AnyLayer::createDenseLayer(In{hidden_size}, Out{output_size}, activation, momentumOpt));
+
+    std::cout << "=== (Momentum) До обучения ===\n";
+    Eigen::MatrixXd pred_before = netMomentum.predict(X);
+    std::cout << "Предсказания:\n" << pred_before << "\n";
+    constexpr int epochs = 100;
+    netMomentum.fit(X, Y, epochs, num_samples, momentumOpt, LossFunction::Euclid(),
+                    Net::NormStatus::NotActive);
+
+    std::cout << "\n=== (Momentum) После обучения (" << epochs << " эпох) ===\n";
+    Eigen::MatrixXd pred_after = netMomentum.predict(X);
+    std::cout << "Предсказания:\n" << pred_after << "\n\n";
+
+    double accuracy = netMomentum.accuracy(X, Y);
+    std::cout << "Точность (Momentum): " << accuracy << "\n\n";
+}
+
+void test_linear_regression_with_adam()
+{
+    std::cout << "----- Тест: Линейная регрессия (y = 3x + 5) с оптимизатором Adam -----\n";
+
+    constexpr Index input_size = 1;
+    constexpr Index hidden_size = 3;
+    constexpr Index output_size = 1;
+    constexpr Index num_samples = 5;
+
+    Eigen::Matrix<double, input_size, num_samples> X;
+    Eigen::Matrix<double, output_size, num_samples> Y;
+    X << 0.0, 1.0, 2.0, 3.0, 4.0;
+    Y << 5.0, 8.0, 11.0, 14.0, 17.0;
+
+    std::cout << "Входные данные X:\n" << X << "\n\n";
+    std::cout << "Целевые данные Y:\n" << Y << "\n\n";
+
+    ActivationFunction activation = ActivationFunction::Identity();
+
+    Optimizer adamOpt = Optimizer::Adam(0.5, 0.9, 0.999, 1e-8);
+    Net netAdam;
+    netAdam.addLayer(
+        AnyLayer::createDenseLayer(In{input_size}, Out{hidden_size}, activation, adamOpt));
+    netAdam.addLayer(
+        AnyLayer::createDenseLayer(In{hidden_size}, Out{output_size}, activation, adamOpt));
+
+    std::cout << "=== (Adam) До обучения ===\n";
+    Eigen::MatrixXd pred_before = netAdam.predict(X);
+    std::cout << "Предсказания:\n" << pred_before << "\n";
+
+    constexpr int epochs = 100;
+    netAdam.fit(X, Y, epochs, num_samples, adamOpt, LossFunction::Euclid(),
+                Net::NormStatus::NotActive);
+
+    std::cout << "\n=== (Adam) После обучения (" << epochs << " эпох) ===\n";
+    Eigen::MatrixXd pred_after = netAdam.predict(X);
+    std::cout << "Предсказания:\n" << pred_after << "\n\n";
+
+    double accuracy = netAdam.accuracy(X, Y);
+    std::cout << "Точность (Adam): " << accuracy << "\n\n";
+}
+
 void run_all_tests()
 {
     test_linear_regression_with_dropout();
-    test_mnist_classification();
+    // test_mnist_classification();
+    // test_mnist_classification_momentum();
+    test_linear_regression_with_momentum();
+    test_linear_regression_with_adam();
 }
 
 }  // namespace neural_network
